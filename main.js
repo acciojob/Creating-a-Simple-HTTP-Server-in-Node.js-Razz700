e14dbc60ef307d897fb588c09cedc56bd53c6856
Razzakh <mrazzakh123@gmail.com>
2024-09-07 14:51:32 +0530

    tenth

--- main.js
+++ main.js
@@ -5,9 +5,8 @@ const res={data:"Hello, World!"}
 const server = http.createServer(async(req, res1) => {
   // TODO: Send the ""Hello, world!"" response
 res1.end('Hello, World!');
-console.log(`res.data: ${res.data}`);
 });
-
+console.log(`res.data: ${res.data}`);
 server.listen(port, () => {
   // TODO: Log a message to the console when the server starts listening on the port
   console.log('Server running on port 3000');
